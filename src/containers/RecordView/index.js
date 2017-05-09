bd7dc063a392036b95863aef7af7f218fcfb2998
ucg8j <lukesingham@gmail.com>
2017-05-09 15:21:48 +1000

    Revert "App prior to src/ replacement from 'update your code' MediaRecorder API"
    This reverts commit 5fddabb5a6aea65ea842414d39d2b932c870a9e0.

--- src/containers/RecordView/index.js
+++ src/containers/RecordView/index.js
@@ -1,64 +1,30 @@
 import React, { Component }   from 'react';
 import Button 								from 'components/Button';
 import MicrophoneIcon 				from 'material-ui/svg-icons/av/mic';
-import DeleteIcon 						from 'material-ui/svg-icons/action/delete';
-import DoneIcon 	 						from 'material-ui/svg-icons/action/done';
-
 import { styles } from './styles.scss';
 
 class RecordView extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    	recording: false
-    }
-  }
-
-  startRecording= () => {
-  	this.setState({
-  		recording: true
-  	});
   }
 
   render() {
   	let buttons;
-		const { recording } = this.state;
 
-  	if(recording) {
-  		buttons = (
-  			<div className="buttons">
-	  			<Button
-		  			className="secondary delete"
-		  			iconOnly={true}
-		  			icon={<DeleteIcon /> }/>
-	  			<Button
-		  			secondary={true}
-		  			raised={true}
-		  			floating={true}
-		  			disabled={true}
-		  			icon={<MicrophoneIcon /> }/>
-	  			<Button
-		  			className="secondary save"
-		  			iconOnly={true}
-		  			icon={<DoneIcon />} />
-	  		</div>
-  		);
-  	} else {
-  		buttons = (
-	  		<Button
-		  		className="btn"
-		  		secondary={true}
-		  		raised={true}
-		  		floating={true}
-		  		onTouchTap={this.startRecording}
-		  		icon={<MicrophoneIcon />} />
-  		);
-  	}
+  	buttons = (
+  		<Button
+	  		className="btn"
+	  		secondary={true}
+	  		raised={true}
+	  		floating={true}
+	  		onTouchTap={this.startRecording}
+	  		icon={<MicrophoneIcon />} />
+  	);
 
     return (
       <div className={styles}>
         <div id="controls">
-        	{buttons}
+        {buttons}
         </div>
       </div>
     );
